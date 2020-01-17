b6a3e9f7b3dc16fca544be0935e53a9f67d3aec6
bartoszborys <borysbartosz1@gmail.com>
2020-01-18 00:00:32 +0100

    initial commit

---
+++ BillStatus/Models/ErrorViewModel.cs
@@ -0,0 +1,13 @@
+using MySql.Data.MySqlClient;
+using System;
+
+namespace BillStatus.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+
+}
